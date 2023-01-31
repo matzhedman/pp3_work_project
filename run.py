73cbfb916df2e56438ea59eb20c52df19e01d90e
matzhedman <hedmanmatz@gmail.com>
2023-01-31 17:36:06 +0000

    test4

--- run.py
+++ run.py
@@ -5,8 +5,6 @@ import datetime
 from google.oauth2.service_account import Credentials
 from colorama import Fore
 
-
-
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -18,9 +16,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('pp3_work_project')
 
-yes_no = ['yes', 'no']
+
+"""
+To run the program: python3 run.py
+"""
+
+
 
 def start():
+    """
+    First view, asks you to type in your name.
+    Name should be logged and be added to the summary in the end - not working yet.
+    """
     print('Hello, welcome to laundry booking!\n')
     print('Please type in your name to book a time,\nor to see current bookings.\n')
     print('____________\n')
@@ -30,13 +37,21 @@ def start():
     print('Enter your name: ')
     name = input()
 
-    def worksheet_name_update(data, worksheet):
-        worksheet_to_update = SHEET.worksheet(worksheet)
-        worksheet_to_update.append_row(data)
+    """
+    Function not working yet
+    """
+    # def worksheet_name_update(data, worksheet):
+    #     worksheet_to_update = SHEET.worksheet(worksheet)
+    #     worksheet_to_update.append_row(data)
 
     
 
 def mainMenu():
+    """
+    Menu. 
+    Shows the day and date of today.
+    Gives three choices to navigate further.
+    """
     os.system('clear')
     x = datetime.datetime.now()
     print('____________\n')
@@ -65,16 +80,14 @@ def mainMenu():
             log_out()
 
       
-#Book a day
+#Choose a day
 def choose_a_day():
     os.system('clear')
     print('Choose a day:\n')
     days = SHEET.worksheet('laundry_days').get_all_values()
 
     for day in days:
-        print(day)
-        
-
+        print(day)      
     while True:
         try:
             choice = int(input('Type in a number to choose a day: \n'))
@@ -96,10 +109,15 @@ def choose_a_day():
         elif choice == 7:
             sunday()
     
-
         
 #Show my bookings menu
 def show_my_booking():
+    """
+    Function not working yet. Bookings should be added to spreadsheet, 
+    when going in to this menu it should get all bookings, if any, from the spreadsheet
+    and show it here. 
+    No bookings? -> a message that says "No current booking"
+    """
     print('Your bookings')
 
 #Log out
@@ -107,23 +125,13 @@ def log_out():
     os.system('clear')
     print(Fore.RED + '\nYou are logged out!')
     print(Fore.WHITE + '____________\n')    
-    start()
-    mainMenu()
-
-#Times
-def times():
-    os.system('clear')
-    print('1) 7.00 AM - 11.00 AM')
-    print('2) 11.00 AM - 3.00 PM')
-    print('3) 3.00 PM - 7.00 PM')
-    print('4) 7.00 PM - 10.00 PM')
+    main()
 
 #Days
 def monday():
     print('\nYou want to do the laundry on Monday')
     print('Please choose a time: ')
     times()
-
 def tuesday():
     print('\nYou want to to the laundry on Tuesday')
     print('Please choose a time: ')
@@ -149,25 +157,44 @@ def sunday():
     print('Please choose a time: ')
     times()
 
+#Times
+def times():
+    os.system('clear')
+    print('1) 7.00 AM - 11.00 AM')
+    print('2) 11.00 AM - 3.00 PM')
+    print('3) 3.00 PM - 7.00 PM')
+    print('4) 7.00 PM - 10.00 PM')
+    while True:
+        try:
+            choice = int(input('Type in number to choose an option: \n'))
+        except ValueError:
+            print("Not a valid number")
+        if choice == 1:
+            print('placeholder1')
+
+        summary()
+
 def summary():
     os.system('clear')
-    print('This is the day\n')
+    print('This is the day "day should appear here"\n')
     print('Would you like to confirm, and go back to Start?')
     answer = input("(yes / no)\n")
     if answer == 'yes':
         os.system('clear')
-        start()
+        main()
     elif answer == 'no':
-        print('ok stay here')
+        print('no-function works')
+        os.system('clear')
+        choose_a_day()
 
 
 def main():
-    start_up = start()
+    """
+    Run all program functions
+    """
+    start_up = start()  
+    menu = mainMenu()
     laundry_times = times()
     summary_end = summary()
-    menu = mainMenu()
-    
-    
-    # back_to = start()
 
 main()
